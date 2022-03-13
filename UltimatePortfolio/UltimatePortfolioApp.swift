d563ae51ac3e3686a459c542d34f5b725596507a
Dan Payne <dpayne@Dans-MacBook-Pro.local>
2022-03-13 08:38:03 -0600

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Dan Payne on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
